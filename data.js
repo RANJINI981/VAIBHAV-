f7c89cf8fff9ca15ae106f9681c4af42ed6e7c1b
RANJINI981 <rraveendran983@gmail.com>
2025-08-02 14:32:29 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,149 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living",
+      "name": "LIVING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.005099251765702917,
+        "pitch": 0.19702812801087433,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1359168722143327,
+          "pitch": 0.9287757813123108,
+          "rotation": 0,
+          "target": "0-living"
+        },
+        {
+          "yaw": -0.14677102484693805,
+          "pitch": 0.3418127015919765,
+          "rotation": 6.283185307179586,
+          "target": "2-foyer-new"
+        },
+        {
+          "yaw": 0.29764592382080224,
+          "pitch": 0.33324512285991403,
+          "rotation": 0,
+          "target": "1-pooja-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pooja-1",
+      "name": "POOJA (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.031515171882585236,
+        "pitch": 0.028557035858460367,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.069527358579986,
+          "pitch": 0.18187661483149498,
+          "rotation": 0,
+          "target": "0-living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-foyer-new",
+      "name": "FOYER NEW",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.1277944127976891,
+        "pitch": 0.013351125861236213,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1274045714387348,
+          "pitch": 0.3784183688157512,
+          "rotation": 0,
+          "target": "0-living"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
